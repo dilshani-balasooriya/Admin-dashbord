f53526ad0cea254aa164bad104a816eee86c0df7
Dilshani Balasooriya <136173570+dilshani-balasooriya@users.noreply.github.com>
2025-02-15 11:51:33 +0530

    Add files via upload

---
+++ dashbord/app/page.tsx
@@ -0,0 +1,36 @@
+import DashboardCard from "@/components/Dashboard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
+import PostsTable from "@/components/Posts/PostsTable";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard
+          title="Posts"
+          count={100}
+          icon={<Newspaper className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Categories"
+          count={12}
+          icon={<Folder className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Users"
+          count={750}
+          icon={<User className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Comments"
+          count={1200}
+          icon={<MessageCircle className="text-slate-500" size={72} />}
+        />
+      </div>
+
+      <PostsTable title="Latest Post "  limit={5}/>
+    </>
+  );
+}
